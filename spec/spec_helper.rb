742842383d50609151ba11dc7a4e65f78f1be871
Denis Tierno <de.tierno@gmail.com>
2014-04-07 11:38:21 -0300

    changed spec helper to require all support files:

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,11 @@
 require 'bundler/setup'
 Bundler.setup
 
-require 'support/vcr'
+Dir[File.dirname(__FILE__) + "/support/*.rb"].each { |f| p f; require f }
+
 require 'webmock/rspec'
 require 'savon'
 require 'tinyerp_ruby'
-require 'support/api_key'
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
