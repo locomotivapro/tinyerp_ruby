81dbfb7bfe3b63f3c3109f6df8cf30a33a064ef6
Denis Tierno <de.tierno@gmail.com>
2014-04-08 15:18:47 -0300

    add filter to token param in log

--- lib/tinyerp_ruby/poster.rb
+++ lib/tinyerp_ruby/poster.rb
@@ -7,7 +7,7 @@ module TinyerpRuby
 
       def call(connection, service, params)
         @connection = connection
-        webservice = Savon.client(wsdl: @connection.base_url, convert_request_keys_to: :none)
+        webservice = Savon.client(wsdl: @connection.base_url, convert_request_keys_to: :none, filters: [:token])
         response = webservice.call(service, message: formatted_params(params))
         parse service, response
       end
